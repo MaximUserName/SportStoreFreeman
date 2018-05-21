c19ce3a47544342daec8b3722f23eb91925b8a42
Maxim Ilyukhin <max86work@gmail.com>
2018-05-22 02:43:07 +0300

    Add Products Pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,11 +10,15 @@ import { Product } from '../model/product.model';
 })
 export class StoreComponent implements OnInit {
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[]{
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[]{
@@ -23,6 +27,21 @@ export class StoreComponent implements OnInit {
 
   changeCategory(newCategory?: string){
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number){
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newPageSize: number){
+    this.productsPerPage = newPageSize;
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[]{
+    return Array(Math.ceil(this.repository
+                  .getProducts(this.selectedCategory).length / this.productsPerPage))
+                  .fill(0).map((x,i) => i + 1);
   }
 
   ngOnInit() {
