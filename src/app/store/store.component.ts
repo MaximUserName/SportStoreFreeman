2da3250908fb0ff4ebd13eb7e7b2d505b87f170c
Maxim Ilyukhin <max86work@gmail.com>
2018-05-22 02:27:30 +0300

    Add store categories menu

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,15 +9,20 @@ import { Product } from '../model/product.model';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+  public selectedCategory = null;
 
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[]{
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
 
   get categories(): string[]{
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string){
+    this.selectedCategory = newCategory;
   }
 
   ngOnInit() {
