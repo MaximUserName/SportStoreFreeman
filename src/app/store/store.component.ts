84fa72308caf6e54539e6a5f0bbd191cad2ade56
Maxim Ilyukhin <max86work@gmail.com>
2018-05-22 19:53:15 +0300

    Products page, pagination, directive. End of 7 chapter

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,50 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductRepository } from '../model/product.repository';
-import { Product } from '../model/product.model';
+import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
 
 @Component({
-  selector: 'store',
-  //moduleId: module.id,
-  templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+    selector: "store",
+    // moduleId: module.id,
+    templateUrl: "store.component.html"
 })
-export class StoreComponent implements OnInit {
-  public selectedCategory = null;
-  public productsPerPage = 4;
-  public selectedPage = 1;
+export class StoreComponent {
+    public selectedCategory = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository) {}
 
-  get products(): Product[]{
-    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
-    return this.repository.getProducts(this.selectedCategory)
-      .slice(pageIndex, pageIndex + this.productsPerPage);
-  }
+    get products(): Product[] {
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
+    }
 
-  get categories(): string[]{
-    return this.repository.getCategories();
-  }
+    get categories(): string[] {
+        return this.repository.getCategories();
+    }
 
-  changeCategory(newCategory?: string){
-    this.selectedCategory = newCategory;
-  }
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
 
-  changePage(newPage: number){
-    this.selectedPage = newPage;
-  }
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
 
-  changePageSize(newPageSize: number){
-    this.productsPerPage = newPageSize;
-    this.changePage(1);
-  }
+    changePageSize(newSize: number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
 
-  get pageNumbers(): number[]{
-    return Array(Math.ceil(this.repository
-                  .getProducts(this.selectedCategory).length / this.productsPerPage))
-                  .fill(0).map((x,i) => i + 1);
-  }
+    get pageCount(): number {
+        return Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage)
+    }
 
-  ngOnInit() {
-  }
-
+    //get pageNumbers(): number[] {
+    //    return Array(Math.ceil(this.repository
+    //        .getProducts(this.selectedCategory).length / this.productsPerPage))
+    //            .fill(0).map((x, i) => i + 1);
+    //}
 }
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { StoreComponent } from "./store.component";
 import { ModelModule } from "../model/model.module";
+import { CounterDirective } from "./counter.directive";
+
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
+    declarations: [StoreComponent, CounterDirective],
     exports: [StoreComponent]
 })
 export class StoreModule { }
+
