686371c75cc17f913d6b4d8258c2b6fb2b1a7f57
Maxim Ilyukhin <max86work@gmail.com>
2018-05-23 02:28:18 +0300

    add rest data source service

---
+++ src/app/model/rest.datasource.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { Http, RequestMethod } from "@angular/http";
+import { Product } from "./product.model";
+import { Order } from "./order.model";
+import { Observable } from "rxjs/Observable";
+
+const PROTOCOL = "http";
+const PORT = 3500;
+
+@Injectable()
+export class RestDataSource {
+    baseUrl: string;
+
+    constructor(private http: Http) {
+        this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+    }
+
+    getProducts(): Observable<Product[]> {
+        return this.sendRequest(RequestMethod.Get, "products");
+    }
+
+    getOrders(): Observable<Order[]> {
+        return this.sendRequest(RequestMethod.Get, "orders");
+    }
+
+    private sendRequest(verb: RequestMethod,
+        url: string, body?: Product | Order): Observable<Product | Order> {
+        return this.http.request(new Request({
+            method: verb,
+            url: this.baseUrl + url,
+            body: body
+        })).map(response => response.json());
+    }
+}
