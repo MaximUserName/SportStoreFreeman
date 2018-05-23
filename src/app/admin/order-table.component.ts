84288a1f228a6526a3c411ada256024c4a7241f0
Maxim Ilyukhin <max86work@gmail.com>
2018-05-23 22:54:30 +0300

    Order table component. Ship, delete order

--- src/app/admin/order-table.component.ts
+++ src/app/admin/order-table.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../model/order.model';
+import { OrderRepository } from '../model/order.repository';
 
 @Component({
   selector: 'app-order-table',
   templateUrl: './order-table.component.html',
   styleUrls: ['./order-table.component.css']
 })
-export class OrderTableComponent implements OnInit {
+export class OrderTableComponent {
 
-  constructor() { }
+  includeShipped: boolean = false;
 
-  ngOnInit() {
+  constructor(private repository: OrderRepository) { }
+
+  getOrders(): Order[]{
+    return this.repository.getOrders()
+      .filter(o => this.includeShipped || !o.shipped);
   }
 
+  markShipped(order: Order){
+    order.shipped = true;
+    this.repository.updateOrder(order);
+  }
+
+  get isEmpty(): boolean {
+    return this.getOrders().length == 0;
+  }
+
+  delete(id: number){
+    this.repository.deleteOrder(id);
+  }
 }
