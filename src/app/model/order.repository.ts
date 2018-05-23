2070dfaf798eaa43a76c30d98e22cf231a2825b9
Maxim Ilyukhin <max86work@gmail.com>
2018-05-23 20:30:47 +0300

    update order, delete order in order repository

--- src/app/model/order.repository.ts
+++ src/app/model/order.repository.ts
@@ -1,21 +1,43 @@
-import { StaticDataSource } from "./static.datasource";
 import { Order } from "./order.model";
 import { Observable } from "rxjs/Observable";
 import { Injectable } from "@angular/core";
+import { RestDataSource } from "./rest.datasource";
 
 @Injectable()
 export class OrderRepository {
     private orders: Order[] = [];
+    private loaded: boolean = false;
 
-    constructor(private dataSource: StaticDataSource){
+    constructor(private dataSource: RestDataSource){
 
     }
 
+    loadOrders(): void {
+        this.loaded = true;
+        this.dataSource.getOrders().subscribe(orders => {
+            this.orders = orders;
+        });
+    }
+
     getOrders(): Order[]{
+        if(!this.loaded)
+            this.loadOrders();
         return this.orders;
     }
 
     saveOrder(order: Order): Observable<Order> {
         return this.dataSource.saveOrder(order);
+    }
+
+    deleteOrder(id: number){
+        this.dataSource.deleteOrder(id).subscribe(order => {
+            this.orders.splice(this.orders.findIndex(o => o.id == id), 1);
+        })
+    }
+
+    updateOrder(order: Order){
+        this.dataSource.updateOrder(order).subscribe(o => {
+            this.orders.splice(this.orders.findIndex(o => o.id == order.id) , 1, o);
+        });
     }
 }
