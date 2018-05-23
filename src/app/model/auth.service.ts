b371280ec004c6709839156c06bddd75fcb5a93e
Maxim Ilyukhin <max86work@gmail.com>
2018-05-23 19:47:50 +0300

    Authentication JWT implemented

---
+++ src/app/model/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { RestDataSource } from './rest.datasource';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private dataSource: RestDataSource) { }
+
+  authenticate(userName: string, password: string): Observable<boolean> {
+    return this.dataSource.authenticate(userName, password);
+  }
+
+  get authenticated(): boolean {
+    return this.dataSource.authenticate != null;
+  }
+
+  clear(){
+    this.dataSource.auth_token = null;
+  }
+}
